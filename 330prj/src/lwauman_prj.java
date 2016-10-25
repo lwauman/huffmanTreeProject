faa4d2282db9627fc985faf75deecd03213a7f00
Lucas Auman <lwauman@uncg.edu>
2016-10-25 11:29:19 -0400

    redoing

--- 330prj/src/lwauman_prj.java
+++ 330prj/src/lwauman_prj.java
@@ -1,76 +1,31 @@
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-public class lwauman_prj {
-    public static void main(String[] args) {
-        
+abstract class HuffTree implements Comparable<HuffTree>{
+    private int frequency;
+    
+    public HuffTree(int freq){
+        frequency = freq;
     }
-    private class HuffNode{
-        int weight;
-        String value;
-        HuffNode parentNode;
-        HuffNode leftNode;
-        HuffNode rightNode;
-        
-        private HuffNode(int w, String v, HuffNode p,HuffNode l, HuffNode r){
-            weight = w;
-            value = v;
-            parentNode = p;
-            leftNode = l;
-            rightNode = r;
-        }
+    @Override
+    public int compareTo(HuffTree tree){
+        return frequency - tree.getFrequency();
     }
-    private static class HuffTree{
-        private HuffNode root;
-        private char[] characters;
-        private HashMap<Character,mapObj> frequency;
-        
-        private HuffTree(String toCompress){
-            characters = new char[toCompress.length()];
-            characters = convertToCharArray(toCompress);
-            findAndSetFreqOfCharacters();
-        }
-        
-        private char[] convertToCharArray(String toConvert){
-            char[] temp = new char[toConvert.length()];
-            for(int i=0; i<toConvert.length(); i++)
-                temp[i] = toConvert.charAt(i);
-            return temp;
-        }
-        
-        private void findAndSetFreqOfCharacters(){
-            frequency = new HashMap<>();
-            for(int i = 0; i<characters.length; i++){
-                if(frequency.containsKey(characters[i]))
-                    frequency.get(characters[i]).incFreq();
-                else
-                    frequency.put(characters[i], new mapObj(characters[i]));
-            }     
-        }
- 
-        private class mapObj{
-            char value;
-            int freq;
-            
-            private mapObj(char val){
-                value = val;
-                freq = 1;
-            }
+    public int getFrequency() {
+        return frequency;
+    }
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    } 
+}
 
-            private char getValue() {
-                return value;
-            }
 
-            private int getFreq() {
-                return freq;
-            }
-            
-            private void incFreq(){
-                freq++;
-            }
+public class lwauman_prj{
+    
+    public class HuffLeaf extends HuffTree{
+        private char value;
+
+        public HuffLeaf(int freq, char val){
+            super(freq);
+            value = val;
         }
     }
+    
 }
