4d07dca7d98fef56e1fb8db45037feac481e96ba
Lucas Auman <lwauman@uncg.edu>
2016-10-25 08:09:41 -0400

    convert to char array and get char frequency

--- 330prj/src/lwauman_prj.java
+++ 330prj/src/lwauman_prj.java
@@ -1,6 +1,12 @@
+
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class lwauman_prj {
     public static void main(String[] args) {
-   
+        HuffTree ht = new HuffTree("abbcd");
+        System.out.println(Arrays.toString(ht.characters));
+        System.out.println(ht.frequency);
     }
     private class HuffNode{
         int weight;
@@ -17,9 +23,33 @@ public class lwauman_prj {
             rightNode = r;
         }
     }
-    private class HuffTree{
-        HuffNode root;
+    private static class HuffTree{
+        private HuffNode root;
+        private char[] characters;
+        private HashMap<Character,Integer> frequency;
         
-        private HuffTree()
+        private HuffTree(String toCompress){
+            characters = new char[toCompress.length()];
+            characters = convertToCharArray(toCompress);
+            findAndSetFreqOfCharacters();
+        }
+        
+        private char[] convertToCharArray(String toConvert){
+            char[] temp = new char[toConvert.length()];
+            for(int i=0; i<toConvert.length(); i++)
+                temp[i] = toConvert.charAt(i);
+            return temp;
+        }
+        
+        private void findAndSetFreqOfCharacters(){
+            frequency = new HashMap<>();
+            for(int i = 0; i<characters.length; i++){
+                if(frequency.containsKey(characters[i]))
+                    frequency.put(characters[i], frequency.get(characters[i])+1);
+                else
+                    frequency.put(characters[i], 1);
+            }
+                
+        }
     }
 }
